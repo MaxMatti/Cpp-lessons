7e5ebfbaec13a92ff6e8ec1ca59b82a263a1719d
Max Staff <max.staff@gmx.de>
2017-04-08 16:55:38 +0200

    Added input and data types to lecture 1

---
+++ examples/02-hello-user.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Please enter your name: ";
+    string name;
+    cin >> name;
+    cout << "Hello " << name << "!" << endl;
+    return 0;
+}
